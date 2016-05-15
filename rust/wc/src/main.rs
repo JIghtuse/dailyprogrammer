2963561d0277e6961a04c7ee83c17874b94eb253
Boris Egorov <egorov@linux.com>
2016-05-15 23:41:10 +0600

    wc: Count words too (37 easy bonus)

--- rust/wc/src/main.rs
+++ rust/wc/src/main.rs
@@ -6,13 +6,23 @@ use std::env;
 
 struct CountStat {
     lines: usize,
+    words: usize,
 }
 
 fn get_count_stat<P: AsRef<Path>>(filename: P) -> Option<CountStat> {
     if let Ok(file) = File::open(filename) {
+        let mut words = 0usize;
+        let mut lines = 0usize;
+
         let reader = BufReader::new(file);
-        let lines = reader.lines().count();
-        Some(CountStat { lines: lines })
+        for line in reader.lines() {
+            words += line.unwrap().split_whitespace().count();
+            lines += 1;
+        }
+        Some(CountStat {
+            lines: lines,
+            words: words,
+        })
     } else {
         None
     }
@@ -24,7 +34,9 @@ fn main() {
 
     if let Some(input_file) = args.next() {
         if let Some(stats) = get_count_stat(&input_file) {
-            println!("Number of lines in {}: {}", &input_file, stats.lines);
+            println!("{} ", &input_file);
+            println!("\tlines: {}", stats.lines);
+            println!("\twords: {}", stats.words);
         }
     } else {
         println!("usage: {} input_file", program_name.unwrap());
