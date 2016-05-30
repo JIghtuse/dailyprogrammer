9b14aba60b6dda57e88242257293601d5d048a07
Boris Egorov <egorov@linux.com>
2016-05-30 23:42:42 +0600

    string_transposition: Reading input

---
+++ rust/string_transposition/src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+
+fn get_number<T>(stdin: &io::Stdin) -> Option<T>
+    where T: std::str::FromStr
+{
+    let mut s = String::new();
+    match stdin.read_line(&mut s) {
+        Err(_) => None,
+        Ok(_) => s.trim().parse::<T>().ok(),
+    }
+}
+
+fn read_words() -> Vec<String> {
+    let stdin = io::stdin();
+    let nwords : usize = get_number(&stdin).unwrap();
+
+    let mut words = vec![];
+
+    for _ in 0..nwords {
+        let mut word = String::new();
+        stdin.read_line(&mut word).unwrap();
+        words.push(word.trim().to_string());
+    }
+    words
+}
+
+fn main() {
+    let words = read_words();
+    let max_length = words.iter().map(|s| s.len()).max().unwrap();
+    println!("max length: {}", max_length);
+}
